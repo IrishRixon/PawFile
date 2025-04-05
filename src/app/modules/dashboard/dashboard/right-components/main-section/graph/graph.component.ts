50b66f99e284ae9bd55e8913bfb3deaa384d3ce8
Irish Rixon Dela Pena <irishrixon@gmail.com>
2025-04-05 16:49:26 +0800

    `feat: added pet profile and graph components to dashboard, updated routing and module imports`

---
+++ src/app/modules/dashboard/dashboard/right-components/main-section/graph/graph.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-graph',
+  standalone: false,
+  
+  templateUrl: './graph.component.html',
+  styleUrl: './graph.component.scss'
+})
+export class GraphComponent {
+
+}
