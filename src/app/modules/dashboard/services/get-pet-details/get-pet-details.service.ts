42c6080b2b9431e83f19e2e34679b16bd413b045
Irish Rixon Dela Pena <irishrixon21delapena@outlook.com>
2025-04-18 15:57:01 +0800

    `Added pet profile details feature: implemented API call to retrieve pet details, updated interfaces and components to display pet profile information.`

--- src/app/modules/dashboard/services/get-pet-details/get-pet-details.service.ts
+++ src/app/modules/dashboard/services/get-pet-details/get-pet-details.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from '../api.service';
+import { Observable } from 'rxjs';
+import { PetProfileDetails } from '../../interfaces/pet-profile-details/pet-profile-details';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetPetDetailsService {
 
-  constructor() { }
+  constructor(private api: ApiService) { }
+
+  getPetDetails = (url: string): Observable<PetProfileDetails> => {
+    return this.api.get(url);
+  }
 }
